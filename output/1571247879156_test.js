14db3b795939faff3d33040dc2ad11ee9ac60259
Chris Kinch <kinchie@gmail.com>
2019-11-04 12:56:36 +0000

    Updating readme

--- output/1571247879156_test.js
+++
@@ -1,8 +0,0 @@
-import testControllerHolder from "/Users/flatworldworks/Codecept/node_modules/codeceptjs/lib/helper/testcafe/testControllerHolder.js";
-
-
-    fixture("fixture")
-
-    test
-
-    ("test", testControllerHolder.capture)
